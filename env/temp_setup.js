dcda00ac8ab78fe8d5dbddbab6f87e51ec92ed01
Madhuri Reddy <mreddy@sparefoot.com>
2019-07-16 21:01:01 -0500

    updated temp_setup.js

--- env/temp_setup.js
+++ env/temp_setup.js
@@ -1,7 +1,15 @@
-const DB_USER = "TODO";
-const DB_PASSWORD = "TODO";
+const DB_HOST = 'localhost';
+const DB_PORT = 5432;
+const DB_URL = 'postgres://localhost:5432/';
+const DB_NAME = 'amz-service';
+const DB_USER = 'TODO';
+const DB_PASSWORD = 'TODO';
 
 module.exports = {
-  user: DB_USER,
-  password: DB_PASSWORD
+  dbHost: DB_HOST,
+  dbPort: DB_PORT,
+  dbUrl: DB_URL,
+  dbName: DB_NAME,
+  dbUser: DB_USER,
+  dbPassword: DB_PASSWORD
 };
